81fb7333ffe20ce08e7212cf66c3214efeced4fb
tomif1 <89856825+tomif1@users.noreply.github.com>
2021-09-20 15:16:44 -0400

    Add files via upload

---
+++ Lab4/hello.py
@@ -0,0 +1,12 @@
+#Oluwatomisin Fayomi / 9/21/2021 made by Al Sweigart
+
+print('Hello world!')
+
+print('What is your name?')
+myName = input()
+print('It is good to meet you',myName)
+print('The length of your name is:',len(myName))
+
+print('What is your age?')
+myAge = input()
+print('You will be',str(int(myAge)+1),'in a year.')
